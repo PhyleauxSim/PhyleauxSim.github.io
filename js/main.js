817f59811cef87ede321122656dcafc07a73c64e
Maame Nyarko <zoesteier1@gmail.com>
2018-12-28 16:16:47 -0500

    removed redraw button

--- js/main.js
+++ js/main.js
@@ -18,9 +18,9 @@ numGenSlider.on("slide", event => {
 });
 function drawCoalescentTree() {
   // Setting plotting defaults
-  var svgWidth = 0.57 * window.innerWidth;
-  var svgHeight = 0.9 * window.innerHeight;
-  var padding = 10;
+  const svgWidth = 0.57 * window.innerWidth;
+  const svgHeight = 0.9 * window.innerHeight;
+  const padding = 10;
 
   const popSize = popSizeSlider.getValue();
   const sampleSize = sampleSizeSlider.getValue();
