b2250ede46abe2170ccaae022547724837a34a14
Maame Nyarko <zoesteier1@gmail.com>
2019-02-19 23:47:41 -0600

    Add sort parameter to drawCoalescentTree function

--- js/main.js
+++ js/main.js
@@ -11,20 +11,55 @@ const numGenSlider = new Slider("#number-of-generations", {
 const sliders = [sampleSizeSlider, popSizeSlider, numGenSlider];
 
 for(let i = 0; i < sliders.length; i++){
-  sliders[i].on('change', function(event){
+  sliders[i].on('slideStop', function(event){
     drawCoalescentTree();
   })
 }
 
-function drawCoalescentTree() {
-  // Setting plotting defaults
-  const svgWidth = 0.57 * window.innerWidth;
-  const svgHeight = 0.9 * window.innerHeight;
-  const padding = 10;
+
+function drawCoalescentTree(sort=false) {
+
+  function clear(){
+    d3.selectAll('svg').remove()
+  }
 
   const popSize = popSizeSlider.getValue();
   const sampleSize = sampleSizeSlider.getValue();
   const nGens = numGenSlider.getValue();
+
+  // Setting plotting defaults
+  const svgWidth = 0.57 * window.innerWidth;
+  const svgHeight = 40 * nGens;           // Height scales with # of generations
+  const padding = 10;
+
+  if (sampleSize > popSize) {
+    alert("Sample size cannot be greater than the population size.");
+    return;
+  }
+
+  clear()
+
+  let history = new coalescentHistory(popSize, nGens, sampleSize);
+
+  history.sampleHistory();
+  sort ? history.drawSortedHistory(svgWidth, svgHeight, padding, "#visualization")  : history.drawHistory(svgWidth, svgHeight, padding, "#visualization")
+
+  const v = new Vivus('vis', {type: 'oneByOne', start: 'autostart', duration: 200, animTimingFunction: Vivus.EASE});
+  
+}
+
+
+function drawAndSortHistory() {
+
+  const popSize = popSizeSlider.getValue();
+  const sampleSize = sampleSizeSlider.getValue();
+  const nGens = numGenSlider.getValue();
+
+  // Setting plotting defaults
+  const svgWidth = 0.57 * window.innerWidth;
+  //const svgHeight = 0.9 * window.innerHeight;   // Constant height
+  const svgHeight = 40 * nGens;           // Height scales with # of generations
+  const padding = 10;
 
   if (sampleSize > popSize) {
     alert("Sample size cannot be greater than the population size.");
@@ -33,11 +68,10 @@ function drawCoalescentTree() {
 
   d3.selectAll("svg").remove();
 
-  let history = new coalescentHistory(popSize, nGens, sampleSize);
+  h = new coalescentHistory(popSize, nGens, sampleSize);
 
-  history.sampleHistory();
+  h.sampleHistory();
 
-  history.drawSortedHistory(svgWidth, svgHeight, padding, "#visualization");
+  h.drawSortedHistory(svgWidth, svgHeight, padding, "#visualization");
 
-   const v = new Vivus('vis', {type: 'oneByOne', start: 'autostart', animTimingFunction: Vivus.EASE})
 }
