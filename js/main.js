db8576d6c08280b0b18cb58857305330acce73cb
Maame Nyarko <zoesteier1@gmail.com>
2019-02-19 23:55:52 -0600

    remove drawAndSortHistory

--- js/main.js
+++ js/main.js
@@ -44,34 +44,5 @@ function drawCoalescentTree(sort=false) {
   history.sampleHistory();
   sort ? history.drawSortedHistory(svgWidth, svgHeight, padding, "#visualization")  : history.drawHistory(svgWidth, svgHeight, padding, "#visualization")
 
-  const v = new Vivus('vis', {type: 'oneByOne', start: 'autostart', duration: 200, animTimingFunction: Vivus.EASE});
-  
-}
-
-
-function drawAndSortHistory() {
-
-  const popSize = popSizeSlider.getValue();
-  const sampleSize = sampleSizeSlider.getValue();
-  const nGens = numGenSlider.getValue();
-
-  // Setting plotting defaults
-  const svgWidth = 0.57 * window.innerWidth;
-  //const svgHeight = 0.9 * window.innerHeight;   // Constant height
-  const svgHeight = 40 * nGens;           // Height scales with # of generations
-  const padding = 10;
-
-  if (sampleSize > popSize) {
-    alert("Sample size cannot be greater than the population size.");
-    return;
-  }
-
-  d3.selectAll("svg").remove();
-
-  h = new coalescentHistory(popSize, nGens, sampleSize);
-
-  h.sampleHistory();
-
-  h.drawSortedHistory(svgWidth, svgHeight, padding, "#visualization");
-
+  const v = new Vivus('vis', {type: 'oneByOne', start: 'autostart', duration: 200, animTimingFunction: Vivus.EASE}); 
 }
